8d7674940164a993f143fbc8b744b5b628df449f
AmiraGhorbel <amiraghorbel72@gmail.com>
2023-02-03 21:30:46 +0100

    Second commit

--- mtc_front/src/App.js
+++ mtc_front/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import AddFiles from './components/AddFiles';
+import './App.css';
+import ListFiles from './components/ListFiles';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Routes>
+          <Route path="/" element={<AddFiles />} />
+          <Route path="/liste" element={<ListFiles />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+//package 1: npm install pdfjs-dist@2.15.349
+//package 2: npm install @react-pdf-viewer/core@3.7.0
+//package 3: npm i @react-pdf-viewer/default-layout
+//package 4: npm i react-router-dom
+//package 5: npm install bootstrap@5.3.0-alpha1
