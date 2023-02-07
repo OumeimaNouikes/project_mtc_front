aa759da793d86d6fd44da06cbf572d6b3f855ff3
unknown <nuiguesoumeima@gmail.com>
2023-02-07 19:53:19 +0100

    chat interface using library socket.io and nodejs

--- mtc_front/src/App.js
+++ mtc_front/src/App.js
@@ -5,15 +5,19 @@ import ListFiles from "./components/ListFiles";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import socketIO from 'socket.io-client';
+import HomeChat from "./components/Homechat"
+import ChatPage from "./components/ChatPage";
+const socket = socketIO.connect('http://localhost:4000');
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<AddFiles />} />
-          <Route path="/liste" element={<ListFiles />} />
-          <Route path="/SignIn" element={<SignIn />} />
-          <Route path="/SignUp" element={<SignUp />} />
+         
+          <Route path="" element={<HomeChat socket={socket}/>}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
         </Routes>
       </Router>
     </div>
